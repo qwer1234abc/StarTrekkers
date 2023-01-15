0ce401119f8522df2eca2ba24ac56cd1bc6e4e2d
qwer1234abc <kelvinbehcy@gmail.com>
2023-01-15 12:29:00 +0800

    adding more functions to interact with the nav

--- js/script.js
+++ js/script.js
@@ -25,3 +25,88 @@ function removeClass(cls, attr) {
     cls.className = cls.className.replace(reg, " ");
   }
 }
+
+function setPos() {
+  wrap.style.transform =
+    "translateX(" + pos_x + "00%) translateY(" + pos_y + "00%)";
+  setTimeout(function () {
+    removeClass(wrap, "animate");
+  }, 600);
+}
+function oriPos() {
+  wrap.style.transform = "translateX(" + 0 + "00%) translateY(" + 0 + "00%)";
+  setTimeout(function () {
+    removeClass(wrap, "animate");
+  }, 600);
+}
+
+setPos();
+
+function moveUp() {
+  addClass(wrap, "animate");
+  pos_y++;
+  setPos();
+}
+
+function moveLeft() {
+  addClass(wrap, "animate");
+  pos_x++;
+  setPos();
+}
+
+function moveRight() {
+  addClass(wrap, "animate");
+  pos_x--;
+  setPos();
+}
+
+function moveDown() {
+  addClass(wrap, "animate-scale");
+  pos_y--;
+  setPos();
+}
+
+for (let x = 0; x < nav_up.length; x++) {
+  nav_up[x].addEventListener("click", moveUp);
+}
+
+for (let x = 0; x < nav_left.length; x++) {
+  nav_left[x].addEventListener("click", moveLeft);
+}
+
+for (let x = 0; x < nav_right.length; x++) {
+  nav_right[x].addEventListener("click", moveRight);
+}
+
+for (let x = 0; x < nav_down.length; x++) {
+  nav_down[x].addEventListener("click", moveDown);
+}
+
+for (let x = 0; x < zoom.length; x++) {
+  zoom[x].addEventListener("click", zoomOut);
+}
+
+function zoomOut(e) {
+  e.stopPropagation();
+  addClass(site, "show-all");
+  for (let x = 0; x < screen.length; x++) {
+    (function (_x) {
+      screen[_x].addEventListener("click", setScreenAndZoom);
+    })(x);
+  }
+  oriPos();
+}
+
+function setScreenAndZoom(e) {
+  (pos_x = -e.target.getAttribute("data-x-pos")),
+    (pos_y = e.target.getAttribute("data-y-pos"));
+  setPos();
+  zoomIn();
+}
+
+function zoomIn() {
+  for (let x = 0; x < screen.length; x++) {
+    screen[x].removeEventListener("click", setScreenAndZoom);
+  }
+  removeClass(site, "show-all");
+}
