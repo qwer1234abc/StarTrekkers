85c4b8445a4f4d2fb788bb9bd245c07e5c693141
bingenk <s10244655@connect.np.edu.sg>
2023-01-29 22:24:10 +0800

    update on user-form js to close form on click

--- js/user-form.js
+++ js/user-form.js
@@ -12,34 +12,37 @@ let SignupForm = document.querySelector(".signup-box");
 let LoginExitIcon = document.querySelector(".exit-icon");
 let SignupExitIcon = document.querySelector(".exit-icon-signup");
 
-let menu = document.querySelector(".screen_zoom")
+let menu = document.querySelector(".screen_zoom");
 // add a click event listener to the login & sign up button
 loginBtn.addEventListener("click", () => {
   // only display the login form
   loginForm.style.display = "block";
-  menu.style.display = "none"
+  menu.style.display = "none";
   const scriptElement = document.querySelector('script[src="js/index.js"]');
   scriptElement.parentNode.removeChild(scriptElement);
 });
 SignupBtn.addEventListener("click", () => {
-  // only display the login form
+  // only display the signup form
   SignupForm.style.display = "block";
-  menu.style.display = "none"
+  menu.style.display = "none";
 });
 // add a click event listener to the exit icon for both login and sign up
 LoginExitIcon.addEventListener("click", () => {
   // only hide the login form
   loginForm.style.display = "none";
-  menu.style.display = "block"
+  menu.style.display = "block";
 });
 SignupExitIcon.addEventListener("click", () => {
-  // only hide the login form
+  // only hide the signup form
   SignupForm.style.display = "none";
-  menu.style.display = "block"
+  menu.style.display = "block";
 });
 
 //Validating
-//Close the login form when yser click on sign up form while login form is still on screen
+//Close the the other form when user click on either form while the other form is still on screen
 SignupBtn.addEventListener("click", function () {
   loginForm.style.display = "none";
+});
+loginBtn.addEventListener("click", function () {
+  SignupForm.style.display = "none";
 });
