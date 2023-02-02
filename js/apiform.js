16a6bfb3c5fdbe94c24e6b9acec6eb9d6921577e
qwer1234abc <kelvinbehcy@gmail.com>
2023-02-02 11:41:37 +0800

    Log in and signup validation done with restdb

--- js/apiform.js
+++ js/apiform.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
   const APIKEY = "63d08564a95709597409cf2d";
+  let login_password;
   function validatePassword(password, confirmpassword) {
     if (password == confirmpassword) {
       return true;
@@ -7,16 +8,14 @@ $(document).ready(function () {
       return false;
     }
   }
-  $("#player-submit").on("click", function (e) {
-    //prevent default action of the button
+  $("#login-submit").on("click", function (e) {
     e.preventDefault();
-    let player_username = $("#player-username").val();
-    let player_password = $("#player-password").val();
-    let form = $("#player-signup-form");
+    $("#login-submit").prop("disabled", true);
+    let login_username = $("#player-login-username").val();
+    login_password = $("#player-login-password").val();
+    let form = $("#player-login-form");
     let inputs = form.find("input");
     let isValid = true;
-    let player_confirm_password = $("#player-confirm-password").val();
-    isValid = validatePassword(player_password, player_confirm_password);
     for (var i = 0; i < inputs.length; i++) {
       if (!inputs[i].value) {
         isValid = false;
@@ -24,37 +23,110 @@ $(document).ready(function () {
       }
     }
     if (!isValid) {
-      alert("Please enter matching passwords");
+      alert("Please field in all inputs");
       e.preventDefault();
       return;
     } else {
-      let jsondata = {
-        username: player_username,
-        password: player_password,
-      };
-      let settings = {
+      var settings = {
         async: true,
         crossDomain: true,
-        url: "https://restdb-efce.restdb.io/rest/players",
-        method: "POST",
+        url: `https://restdb-efce.restdb.io/rest/players?q={"username":"${login_username}"}`,
+        method: "GET",
         headers: {
           "content-type": "application/json",
           "x-apikey": APIKEY,
           "cache-control": "no-cache",
         },
-        processData: false,
-        data: JSON.stringify(jsondata),
-        beforeSend: function () {
-          $("#player-submit").prop("disabled", true);
-        },
       };
       $.ajax(settings).done(function (response) {
-        console.log(response);
-        $("#player-submit").prop("disabled", false);
-        alert("Player record successfully added");
-        $("#player-signup-form").trigger("reset");
-
+        if (response.length === 0) {
+          // Show an error message if the username does not exist
+          alert("Username does not exist");
+        } else if (response[0].password !== login_password) {
+          // Show an error message if the password is incorrect
+          alert("Password is incorrect");
+        } else {
+          alert("Successfully signed in");
+          $("#login-submit").prop("disabled", false);
+          $("#player-login-form").trigger("reset");
+          loginForm.style.display = "none";
+        }
       });
+    }
+  });
+
+  $("#signup-submit").on("click", function (e) {
+    //prevent default action of the button
+    e.preventDefault();
+    let player_username = $("#player-username").val();
+    let player_password = $("#player-password").val();
+    let form = $("#player-signup-form");
+    let inputs = form.find("input");
+    let passwordCheck = true;
+    let formCheck = true;
+    let player_confirm_password = $("#player-confirm-password").val();
+    passwordCheck = validatePassword(player_password, player_confirm_password);
+    for (var i = 0; i < inputs.length; i++) {
+      if (!inputs[i].value) {
+        formCheck = false;
+        break;
+      }
+    }
+    if (!formCheck) {
+      alert("Please field in all inputs");
+      e.preventDefault();
+      return;
+    } else {
+      if (!passwordCheck) {
+        alert("Please enter matching passwords");
+        e.preventDefault();
+        return;
+      } else {
+        var settings = {
+          async: true,
+          crossDomain: true,
+          url: `https://restdb-efce.restdb.io/rest/players?q={"username":"${player_username}"}`,
+          method: "GET",
+          headers: {
+            "content-type": "application/json",
+            "x-apikey": APIKEY,
+            "cache-control": "no-cache",
+          },
+        };
+        $.ajax(settings).done(function (response) {
+          if (response.length > 0) {
+            alert("Username taken already taken, please choose another one");
+          } else {
+            let jsondata = {
+              username: player_username,
+              password: player_password,
+            };
+            let settings = {
+              async: true,
+              crossDomain: true,
+              url: "https://restdb-efce.restdb.io/rest/players",
+              method: "POST",
+              headers: {
+                "content-type": "application/json",
+                "x-apikey": APIKEY,
+                "cache-control": "no-cache",
+              },
+              processData: false,
+              data: JSON.stringify(jsondata),
+              beforeSend: function () {
+                $("#signup-submit").prop("disabled", true);
+              },
+            };
+            $.ajax(settings).done(function (response) {
+              console.log(response);
+              $("#signup-submit").prop("disabled", false);
+              alert("Registration successful!");
+              $("#player-signup-form").trigger("reset");
+              SignupForm.style.display = "none";
+            });
+          }
+        });
+      }
     }
   });
 });
