3856c5855e80806c86d4cbd68da29af34e1ddf09
qwer1234abc <kelvinbehcy@gmail.com>
2023-02-09 11:11:07 +0800

    removing pasword for a while

--- js/shop.js
+++ js/shop.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
   };
 
   $.ajax(settings).done(function (response) {
-    for (var i = 0; i < response.length && i < 5; i++) {
+    for (var i = 0; i < response.length; i++) {
       var item = response[i];
       var shortName = item.name
         .split(" ")
@@ -21,7 +21,7 @@ $(document).ready(function () {
         })
         .join("");
       $(".shop-container").append(
-        '<div class="card">' +
+        `<div class="card" data-id="${item._id}">` +
           '<h3 class="title">' +
           shortName +
           "</h3>" +
@@ -37,6 +37,36 @@ $(document).ready(function () {
           "</div>" +
           "</div>"
       );
+    }
+  });
+  $(".shop-container").on("click", ".buy-btn", function () {
+    var button = $(this);
+    if (!localStorage.getItem("loggedIn")) {
+      alert("Please log in to access the shop.");
+      return;
+    }
+    var id = $(this).closest(".card").data("id");
+    console.log(id);
+    button.prop("disabled", true);
+    if (confirm("Confirm purchase?")) {
+      alert("Item will soon be sent to your mailbox. Thank you!")
+      var settings = {
+        async: true,
+        crossDomain: true,
+        url: "https://testa-6711.restdb.io/rest/shop/" + id,
+        method: "DELETE",
+        headers: {
+          "content-type": "application/json",
+          "x-apikey": "63e29704478852088da67e89",
+          "cache-control": "no-cache",
+        },
+      };
+      $.ajax(settings).done(function (response) {
+        console.log(response);
+        location.reload();
+      });
+    } else {
+      button.prop("disabled", false);
     }
   });
 });
