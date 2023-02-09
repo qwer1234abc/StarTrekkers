84b9bf98187e82992145e11e04b25c961a22e587
qwer1234abc <kelvinbehcy@gmail.com>
2023-02-09 11:26:11 +0800

    encrpyt password in itempost.html

--- js/post.js
+++ js/post.js
@@ -1,58 +1,73 @@
-fetch("https://fakestoreapi.com/products")
-  .then((response) => response.json())
-  .then((json) => {
-    var password = prompt("Enter password to post data.");
-    if (password === "123") {
-      if (Array.isArray(json)) {
-        json.forEach((item) => {
-          var jsondata = {
-            item_name: item.title,
-            item_price: item.price,
-            item_image: item.image,
-          };
-          var settings = {
-            async: true,
-            crossDomain: true,
-            url: "https://restdb-efce.restdb.io/rest/shop",
-            method: "POST",
-            headers: {
-              "content-type": "application/json",
-              "x-apikey": "63d08564a95709597409cf2d",
-              "cache-control": "no-cache",
-            },
-            processData: false,
-            data: JSON.stringify(jsondata),
-          };
-          $.ajax(settings).done(function (response) {
-            console.log(response);
-          });
-        });
-      } else {
-        var jsondata = {
-          item_name: json.title,
-          item_price: json.price,
-          item_image: json.image,
-        };
-        var settings = {
-          async: true,
-          crossDomain: true,
-          url: "https://restdb-efce.restdb.io/rest/shop",
-          method: "POST",
-          headers: {
-            "content-type": "application/json",
-            "x-apikey": "63d08564a95709597409cf2d",
-            "cache-control": "no-cache",
-          },
-          processData: false,
-          data: JSON.stringify(jsondata),
-        };
-        $.ajax(settings).done(function (response) {
-          console.log(response);
-        });
-      }
-    }
-    else
-    {
-        alert("Password incorrect")
-    }
-  });
+$(document).ready(function () {
+  fetch("https://fakestoreapi.com/products")
+    .then((response) => response.json())
+    .then((json) => {
+      var password = "bingdog";
+      var hash = crypto.subtle.digest(
+        "SHA-256",
+        new TextEncoder().encode(password)
+      );
+
+      $.when(hash).then(function (result) {
+        var hex = Array.prototype.map
+          .call(new Uint8Array(result), function (x) {
+            return ("00" + x.toString(16)).slice(-2);
+          })
+          .join("");
+        var expectedHash = hex;
+        var inputPassword = prompt("Dont allow giga randoms especially the M:");
+
+        if (inputPassword == expectedHash) {
+          if (Array.isArray(json)) {
+            json.forEach((item) => {
+              var jsondata = {
+                name: item.title,
+                price: item.price,
+                image: item.image,
+              };
+              var settings = {
+                async: true,
+                crossDomain: true,
+                url: "https://testa-6711.restdb.io/rest/shop",
+                method: "POST",
+                headers: {
+                  "content-type": "application/json",
+                  "x-apikey": "63e29704478852088da67e89",
+                  "cache-control": "no-cache",
+                },
+                processData: false,
+                data: JSON.stringify(jsondata),
+              };
+              $.ajax(settings).done(function (response) {
+                console.log(response);
+              });
+            });
+          } else {
+            var jsondata = {
+              name: json.title,
+              price: json.price,
+              image: json.image,
+            };
+            var settings = {
+              async: true,
+              crossDomain: true,
+              url: "https://testa-6711.restdb.io/rest/shop",
+              method: "POST",
+              headers: {
+                "content-type": "application/json",
+                "x-apikey": "63e29704478852088da67e89",
+                "cache-control": "no-cache",
+              },
+              processData: false,
+              data: JSON.stringify(jsondata),
+            };
+            $.ajax(settings).done(function (response) {
+              console.log(response);
+            });
+          }
+        } else {
+          alert("GTFO");
+        }
+      });
+    });
+});
